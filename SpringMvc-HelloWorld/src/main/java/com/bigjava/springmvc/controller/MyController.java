55504d8dfa2ced978825083a8f2bce94aeaa6c43
zgp <2088220905@qq.com>
2021-02-25 15:53:29 +0800

    SpringMvc-HelloWorld

---
+++ SpringMvc-HelloWorld/src/main/java/com/bigjava/springmvc/controller/MyController.java
@@ -0,0 +1,31 @@
+package com.bigjava.springmvc.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author zgp
+ * @Since 2021 -02 -25 10 :15
+ * @Description
+ */
+public class MyController implements Controller {
+
+    /**
+     * 这个方法用来处理请求
+     * @param httpServletRequest 这个就是前端发送来的请求
+     * @param httpServletResponse 这个就是服务端给前端的响应
+     * @return 返回 ModelAndView Model: 就是相当于我们的数据模型，View：则是视图
+     * @throws Exception
+     */
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        //hello: 指定视图名称
+        ModelAndView modelAndView=new ModelAndView("hello");
+        modelAndView.addObject("name","Lisi");
+        modelAndView.addObject("sex","man");
+        modelAndView.addObject("age",18);
+        return modelAndView;
+    }
+}
---
+++ SpringMvc-HelloWorld/src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zgp
+  Date: 2021/2/25
+  Time: 10:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    name:${name}
+    name:${sex}
+    name:${age}
+</body>
+</html>
