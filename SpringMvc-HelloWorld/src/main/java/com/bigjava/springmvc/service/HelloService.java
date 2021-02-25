2d5406487475f7e5ffa7e0dc393780e7c7ab0936
zgp <2088220905@qq.com>
2021-02-25 16:31:47 +0800

    Add Spring

---
+++ SpringMvc-HelloWorld/src/main/java/com/bigjava/springmvc/service/HelloService.java
@@ -0,0 +1,17 @@
+package com.bigjava.springmvc.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author zgp
+ * @Since 2021 -02 -25 16 :22
+ * @Description
+ */
+@Service
+public class HelloService {
+
+    public String helloService(String name){
+
+        return "hello"+name;
+    }
+}
